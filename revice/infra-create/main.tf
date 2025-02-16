f3b9307193abfdf7a6e064b16b5bfd680cfd76ba
manupanand <manupanandindianinvestor@gmail.com>
2025-02-16 17:48:29 +0530

    update gh runner code infra

---
+++ revice/infra-create/main.tf
@@ -0,0 +1,31 @@
+resource "aws_instance" "tool" {
+  ami=data.aws_ami.aws-rhel-ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.tool-sg.ids]
+  tags={
+    Name= var.name
+  }
+}
+
+resource "aws_security_group" "tool-sg" {
+  name= "${var.name}-sg"
+  description = "${var.name}-sg"
+
+  egress = {
+    from_port= 0
+    to_port=0
+    protocol="-1"
+    cidr_blocks= ["0.0.0.0/0"]
+  }
+  ingress = {
+    from_port= 22
+    to_port=22
+    protocol="TCP"
+    cidr_blocks= ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name="${var.name}-sg"
+  }
+
+}
