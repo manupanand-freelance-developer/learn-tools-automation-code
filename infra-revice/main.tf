258cd3d0e6a1908f16ba222838087ca756df4084
manupanand <manupanandindianinvestor@gmail.com>
2025-02-19 22:08:11 +0530

    update code

--- infra-revice/main.tf
+++ infra-revice/main.tf
@@ -1,9 +1,10 @@
 resource "aws_instance" "name" {
+  for_each = var.tools
     ami=data.aws_ami.ami-data.image_id
     instance_type = "t3.micro"
     
   tags = {
-    Name="test-server"
+    Name="${each.key}-server"
   }
 }
 
