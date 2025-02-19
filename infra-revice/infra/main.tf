22b7756963382b375622f3aa2e6aed6b89bbf364
manupanand <manupanandindianinvestor@gmail.com>
2025-02-19 23:21:51 +0530

    updated code

--- infra-revice/infra/main.tf
+++ infra-revice/infra/main.tf
@@ -10,17 +10,18 @@ resource "aws_instance" "tool" {
 
 
 resource "aws_route53_record" "record-public" {
+ 
   zone_id = var.zone_id
-  name = "${each.key}.${var.domain_name}"
+  name = "${var.name}.${var.domain_name}"
   type = "A"
   ttl  = 25
-  records = [aws_instance.tool[each.key].public_ip]
+  records = [aws_instance.tool.public_ip]
   
 }
 resource "aws_route53_record" "record-internal" {
   zone_id = var.zone_id
-  name = "${each.key}.internal.${var.domain_name}"
+  name = "${var.name}.internal.${var.domain_name}"
   type = "A"
   ttl  = 25
-  records = [aws_instance.tool[each.key].private_ip]
+  records = [aws_instance.tool.private_ip]
 }
