873eacf573f06a952a8c7edfa72989aa7cc853f0
manupanand <manupanandindianinvestor@gmail.com>
2025-02-20 18:35:57 +0530

    update ingress

--- infra-revice/infra/main.tf
+++ infra-revice/infra/main.tf
@@ -31,13 +31,13 @@ resource "aws_instance" "tool" {
 resource "aws_security_group" "tools_security_group" {
   name="${var.name}-sg"
   description = "${var.name}-security-group"
-  egress={
+  egress{
     from_port = 0
     to_port   = 0
     protocol  = "-1"
     cidr_blocks=["0.0.0.0/0"]
   }
-  ingress={
+  ingress{
       from_port = 22
       to_port   = 22
       protocol  = "TCP"
