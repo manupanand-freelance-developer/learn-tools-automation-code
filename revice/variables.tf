36e82330c31a895d0fb7665a0818884750a63604
manupanand <manupanandindianinvestor@gmail.com>
2025-03-11 19:03:04 +0530

    update infra provisoning for tools

--- revice/variables.tf
+++ revice/variables.tf
@@ -15,11 +15,19 @@ variable "tools" {
         }
         volume_size=20
     }
-     minikube={
-        instance_type="t3.medium"
+    #  minikube={
+    #     instance_type="t3.medium"
+    #     policy_name= []
+    #     ports={
+    #       kube=8443
+    #     }
+    #     volume_size=30
+    # }
+     elk={
+        instance_type="r7i.large"
         policy_name= []
         ports={
-          kube=8443
+          vault=80
         }
         volume_size=30
     }
